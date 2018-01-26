973a6e461a12c4144df96d6f470ba86b3c04c997
Hugo Montegano <sardone666@gmail.com>
2018-01-26 22:52:14 +0100

    Version de base

---
+++ Assets/Scripts/PigeonMouvement.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PigeonMouvement : MonoBehaviour {
+
+	public float L_speed;
+	private float y;
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	void FixedUpdate () {
+		Move ();
+	}
+
+	private void Move(){
+		y = Input.GetAxis ("Vertical") * Time.deltaTime * L_speed;
+		transform.Translate (0, y, 0);
+	}
+}
