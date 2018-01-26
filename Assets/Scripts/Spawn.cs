8180954c209d29832c45e77c69ea2899eb0cde67
Hugo Montegano <sardone666@gmail.com>
2018-01-26 23:48:03 +0100

    ajout d'un ennemi frontale

---
+++ Assets/Scripts/Spawn.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawn : MonoBehaviour {
+
+	public GameObject[] L_spawnPoint;
+	private 
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
