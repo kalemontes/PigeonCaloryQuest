a0aa7ce9b246a4d0a25c5b24ee9fb9f3eaa6e25a
Hugo Montegano <sardone666@gmail.com>
2018-01-27 08:59:52 +0100

    Spawn fonction

--- Assets/Scripts/Spawn.cs
+++ Assets/Scripts/Spawn.cs
@@ -4,15 +4,51 @@ using UnityEngine;
 
 public class Spawn : MonoBehaviour {
 
-	public GameObject[] L_spawnPoint;
-	private 
+	public GameObject prefab;
+	private GameObject[] frontEnnemi;
+	private int nbMaxEnnemi;
+	private int nbEnnemiEnVie;
+	private int indice;
 	// Use this for initialization
+
+
 	void Start () {
-		
+		//Vector3 pos = spawn.transform.position;
+		nbMaxEnnemi = 3;
+		nbEnnemiEnVie = 0;
+		frontEnnemi = new GameObject[nbMaxEnnemi];
+		for (indice = 0; indice < nbMaxEnnemi; indice++) {
+			frontEnnemi [indice] = Instantiate (prefab, new Vector3(0, 0, 0), Quaternion.identity);
+			frontEnnemi [indice].gameObject.SetActive (false);
+		}
+		indice = 0;
+		InvokeRepeating ("ActiverEnnemi", 0.1f, 0.1f);
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
+	}
+
+	void FixedUpdate(){
+		
+	}
+
+	private void ActiverEnnemi(){
+		if (nbEnnemiEnVie <= nbMaxEnnemi) {
+			if (indice < frontEnnemi.Length) {
+				frontEnnemi [indice].GetComponent<FrontEnnemi> ().Reint ();
+				Debug.Log ("apparition de l'ennemi " + frontEnnemi[indice]);
+				nbEnnemiEnVie++;
+				indice++;
+			} else {
+				indice = 0;
+			}
+		}
+	}
+
+	public void EnnemiMort(){
+		nbEnnemiEnVie--;
+		Debug.Log ("ennemi tué");
 	}
 }
