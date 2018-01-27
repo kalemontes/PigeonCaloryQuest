4ba5cff8e7be48db075fc30133893b58f417c7aa
Camilo Montes <ale8689@gmail.com>
2018-01-27 09:02:14 +0100

    Refactorisation et ajout de certains comportements du Pigeon et FrontEnnemi

---
+++ Assets/IAEnnemi.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class IAEnnemi : MonoBehaviour {
+
+    public float L_speed;
+    public float calories;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void FixedUpdate() {
+        Move();
+    }
+
+    protected abstract void Move();
+
+}
--- Assets/Scripts/PigeonMouvement.cs
+++ Assets/Scripts/PigeonMouvement.cs
@@ -2,21 +2,57 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PigeonMouvement : MonoBehaviour {
+public class PigeonMouvement : MonoBehaviour
+{
 
-	public float L_speed;
-	private float y;
-	// Use this for initialization
-	void Start () {
-		
-	}
+    public float speed;
+    public float weight;
+    private float y;
 
-	void FixedUpdate () {
-		Move ();
-	}
+    // Use this for initialization
+    void Start()
+    {
+        //TODO: rotater = GameObject.Find("Cube"); comprendre ce que ça fait ??
+    }
 
-	private void Move(){
-		y = Input.GetAxis ("Vertical") * Time.deltaTime * L_speed;
-		transform.Translate (0, y, 0);
-	}
+    void FixedUpdate()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        y = Input.GetAxis("Vertical") * Time.deltaTime * speed;
+        transform.Translate(0, y, 0);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Une collision avec quelque chose");
+        bool didCollideWithEnnemi = handleCollisionWithFrontEnnemi(other);
+        if(didCollideWithEnnemi)
+        {
+            applyScaledWeight();
+        }
+    }
+
+    private bool handleCollisionWithFrontEnnemi(Collider other)
+    {
+        bool didHandle = false;
+        FrontEnnemi ennemi = other.GetComponent<FrontEnnemi>();
+        if (ennemi)
+        {
+            Debug.Log("Collision avec un FrontEnnemi detectée");
+            Debug.Log("Le pigeon prendra un poid de + " + ennemi.calories);
+            this.weight += ennemi.calories;
+            didHandle = true;
+        }
+        return didHandle;
+    }
+
+    private void applyScaledWeight()
+    {
+        //TODO: faire l'echelle en fonction du poids du pigeon
+        gameObject.transform.localScale = new Vector3(2, 2, 1);
+    }
 }
